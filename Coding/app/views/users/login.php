3df1df55fca202972d37d31c1eb376bba87fa3bb
SOH HAN WEI <weihansoh2030@gmail.com>
2023-12-19 12:26:19 +0800

    Update login.php

--- Coding/app/views/users/login.php
+++ Coding/app/views/users/login.php
@@ -61,19 +61,15 @@
                             <!--begin::Heading-->
                             <div class="text-center mb-11">
                                 <!--begin::Title-->
-                                <h1 class="text-gray-900 fw-bolder mb-3">Sign In</h1>
+                                <h1 class="text-gray-900 fw-bolder mb-3">Log In Account</h1>
                                 <!--end::Title-->
-                                <!--begin::Subtitle-->
-                                <div class="text-gray-500 fw-semibold fs-6">Your  Campaigns</div>
-                                <!--end::Subtitle=-->
                             </div>
                             <!--begin::Heading-->
                          
-                       
                             <!--begin::Input group=-->
                             <div class="fv-row mb-8">
                                 <!--begin::Email-->
-                                <input type="text" placeholder="username" name="username" autocomplete="off"
+                                <input type="text" placeholder="Username" name="username" autocomplete="off"
                                     class="form-control bg-transparent" />
                                 <!--end::Email-->
                               
@@ -83,6 +79,7 @@
                              
                             </div>
                             <!--end::Input group=-->
+
                             <div class="fv-row mb-3">
                                 <!--begin::Password-->
                                 <input type="password" placeholder="Password" name="password" autocomplete="off"
@@ -98,13 +95,13 @@
                                 <div></div>
                                 <!--begin::Link-->
                                 <a href="authentication/layouts/corporate/reset-password.html"
-                                    class="link-primary">Forgot Password ?</a>
+                                    class="link-primary">Forgot Password?</a>
                                 <!--end::Link-->
                             </div>
                             <!--end::Wrapper-->
                             <!--begin::Submit button-->
                             <div class="d-grid mb-10">
-                                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+                                <button type="submit" id="kt_sign_in_submit" class="btn btn-danger">
                                     <!--begin::Indicator label-->
                                     <span class="indicator-label">Sign In</span>
                                     <!--end::Indicator label-->
@@ -116,7 +113,7 @@
                             </div>
                             <!--end::Submit button-->
                             <!--begin::Sign up-->
-                            <div class="text-gray-500 text-center fw-semibold fs-6">Not a Member yet?
+                            <div class="text-gray-500 text-center fw-semibold fs-6">Register an account?
                                 <a href="<?php echo URLROOT; ?>/users/register" class="link-primary">Sign up</a>
                             </div>
                             <!--end::Sign up-->
@@ -131,29 +128,25 @@
             <!--end::Body-->
             <!--begin::Aside-->
             <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
-                style="background-image: url(<?php echo URLROOT ?>/public/assets/media/misc/auth-bg.png)">
+                style="background-image: url(<?php echo URLROOT ?>/public/assets/media/misc/auth-bg.jpg)">
                 <!--begin::Content-->
                 <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
                     <!--begin::Logo-->
                     <a href="index.html" class="mb-0 mb-lg-12">
-                        <img alt="Logo" src="<?php echo URLROOT ?>/public/assets/media/logos/custom-1.png" class="h-60px h-lg-75px" />
+                        <img alt="Logo" src="<?php echo URLROOT ?>/public/assets/media/logos/YV logo.png" class="h-60px h-lg-75px" />
                     </a>
                     <!--end::Logo-->
                     <!--begin::Image-->
                     <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20"
-                        src="<?php echo URLROOT ?>/public/assets/media/misc/auth-screens.png" alt="" />
+                        src="<?php echo URLROOT ?>/public/assets/media/misc/auth-screens.jpeg" alt="" />
                     <!--end::Image-->
                     <!--begin::Title-->
-                    <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">Fast, Efficient and
-                        Productive</h1>
+                    <h1 class="d-none d-lg-block text-white fs-1 fw-bolder text-center mb-7">Where Passionate Youths Pursue Their Ideas!</h1>
                     <!--end::Title-->
                     <!--begin::Text-->
-                    <div class="d-none d-lg-block text-white fs-base text-center">In this kind of post,
-                        <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the blogger</a>introduces a
-                        person they’ve interviewed
-                        <br />and provides some background information about
-                        <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>and their
-                        <br />work following this is a transcript of the interview.
+                    <div class="d-none d-lg-block text-white fs-3 fs-base text-center">
+                        Subscribe and be alone no more. We will help you with your<br>
+                        entrepreneurial journey before and after graduation.
                     </div>
                     <!--end::Text-->
                 </div>
