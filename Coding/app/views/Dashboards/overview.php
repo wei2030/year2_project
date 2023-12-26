9322adf2dc0db697ed7c48a186f539c176709957
SOH HAN WEI <weihansoh2030@gmail.com>
2023-12-26 16:55:23 +0800

    Update overview.php

--- Coding/app/views/Dashboards/overview.php
+++ Coding/app/views/Dashboards/overview.php
@@ -1,13 +1,69 @@
 <div class="card">
 
+    <!-- List number of user -->
     <div class="row">
-        <!-- left side -->
-        <div class="col-md-6">
+        <!-- Pie chart for all user -->
+        <div class="col-md-4">
+            <img class="mx-auto card-rounded-bottom" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/pie.png"/>
+        </div>
+        <!-- End of pie chart -->
+
+        <!-- List of number each user -->
+        <div class="col-md-8">
+            <!-- Row for number of student -->
             <div class="row">
-                <!-- Link to Manage Activity -->
                 <p class="card hover-elevate-up shadow-sm">
                     <div class="card-body d-flex align-items">
-                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/activity icon.png"/>
+                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="student" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/student.png"/>
+
+                        <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Number of student registered
+                        </span>
+                    </div>
+                </p>
+            </div>
+            <!-- End of row -->
+
+            <!-- Row for number of lecturer -->
+            <div class="row">
+                <p class="card hover-elevate-up shadow-sm">
+                    <div class="card-body d-flex align-items">
+                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="lecturer" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/lecturer.png"/>
+
+                        <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Number of lecturer registered
+                        </span>
+                    </div>
+                </p>
+            </div>
+            <!-- End of row -->
+
+            <!-- Row for number of organizer -->
+            <div class="row">
+                <p class="card hover-elevate-up shadow-sm">
+                    <div class="card-body d-flex align-items">
+                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="organizer" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/organizer.png"/>
+
+                        <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Number of organizer registered
+                        </span>
+                    </div>
+                </p>
+            </div>
+            <!-- End of row -->
+        </div>
+        <!-- End of list -->
+    </div>
+    <!-- End of list number of user -->
+
+    <div class="row">
+        <!-- left side -->
+        <div class="col-md-5">
+            <!-- Row for number of skill -->
+            <div class="row">
+                <p class="card hover-elevate-up shadow-sm">
+                    <div class="card-body d-flex align-items">
+                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/skill.png"/>
 
                         <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Number of skills created
@@ -15,12 +71,13 @@
                     </div>
                 </p>
             </div>
+            <!-- End of row -->
 
+            <!-- Row for number of badges -->
             <div class="row">
-                <!-- Link to Manage Activity -->
                 <p class="card hover-elevate-up shadow-sm">
                     <div class="card-body d-flex align-items">
-                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/activity icon.png"/>
+                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/badge.png"/>
 
                         <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Number of badges created
@@ -28,12 +85,13 @@
                     </div>
                 </p>
             </div>
+            <!-- End of row -->
 
+            <!-- Row for number of registered -->
             <div class="row">
-                <!-- Link to Manage Activity -->
                 <p class="card hover-elevate-up shadow-sm">
                     <div class="card-body d-flex align-items">
-                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/activity icon.png"/>
+                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/form.png"/>
 
                         <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Total number of registered in all activities
@@ -41,17 +99,19 @@
                     </div>
                 </p>
             </div>
+            <!-- End of row -->
         </div>
+        <!-- End of left side -->
 
 
         <!-- right side -->
-        <div class="col-md-6">
+        <div class="col-md-7">
             <div class="row">
                 <div class="col-md-6">
                     <!-- Link to Manage Activity -->
                     <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
                         <div class="card-body d-flex align-items">
-                            <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="activity icon" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/activity icon.png"/>
+                            <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="activity icon" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/activity.png"/>
 
                             <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
                                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Manage Activity
@@ -64,7 +124,7 @@
                     <!-- Link to List of Reward -->
                     <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
                         <div class="card-body d-flex align-items">
-                            <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="reward icon" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/reward icon.png"/>
+                            <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="reward icon" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/reward.png"/>
 
                             <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
                                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;List of Rewards
@@ -72,6 +132,10 @@
                         </div>
                     </a>
                 </div>
+            </div>
+
+            <div class="row">
+                <img class="card-rounded-bottom" alt="leaderboard" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/leaderboard exp.jpg"/>
             </div>
             <!-- <div class="col">
                 // Link to Leaderboard 
@@ -86,6 +150,7 @@
                 </a>
             </div> -->
         </div>
+        <!-- End of right side -->
     </div>
 
 </div>
