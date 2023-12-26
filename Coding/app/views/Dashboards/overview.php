2d4851e4c2e0b34dd6e9fd01f7640c3befe2597e
SOH HAN WEI <weihansoh2030@gmail.com>
2023-12-26 14:40:50 +0800

    Update overview.php

--- Coding/app/views/Dashboards/overview.php
+++ Coding/app/views/Dashboards/overview.php
@@ -1,37 +1,80 @@
-<div class="card shadow-sm">
-    
-    <div class="card-body">
+<div class="card">
 
-        <div class="row">
-
-            <div class="col-md-4">
+    <div class="row">
+        <!-- left side -->
+        <div class="col-md-6">
+            <div class="row">
                 <!-- Link to Manage Activity -->
-                <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
+                <p class="card hover-elevate-up shadow-sm">
                     <div class="card-body d-flex align-items">
                         <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/activity icon.png"/>
 
                         <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Manage Activity
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Number of skills created
                         </span>
                     </div>
-                </a>
+                </p>
             </div>
 
-            <div class="col-md-4">
-                <!-- Link to List of Reward -->
-                <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
+            <div class="row">
+                <!-- Link to Manage Activity -->
+                <p class="card hover-elevate-up shadow-sm">
                     <div class="card-body d-flex align-items">
-                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/reward icon.png"/>
+                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/activity icon.png"/>
 
                         <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;List of Rewards
-                           </span>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Number of badges created
+                        </span>
                     </div>
-                </a>
+                </p>
             </div>
 
-            <div class="col-md-4">
-                <!-- Link to Leaderboard -->
+            <div class="row">
+                <!-- Link to Manage Activity -->
+                <p class="card hover-elevate-up shadow-sm">
+                    <div class="card-body d-flex align-items">
+                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/activity icon.png"/>
+
+                        <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Total number of registered in all activities
+                        </span>
+                    </div>
+                </p>
+            </div>
+        </div>
+
+
+        <!-- right side -->
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-6">
+                    <!-- Link to Manage Activity -->
+                    <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
+                        <div class="card-body d-flex align-items">
+                            <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="activity icon" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/activity icon.png"/>
+
+                            <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Manage Activity
+                            </span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-md-6">
+                    <!-- Link to List of Reward -->
+                    <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
+                        <div class="card-body d-flex align-items">
+                            <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="reward icon" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/reward icon.png"/>
+
+                            <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;List of Rewards
+                            </span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <!-- <div class="col">
+                // Link to Leaderboard 
                 <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
                     <div class="card-body d-flex align-items">
                         <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/leaderboard icon.png"/>
@@ -41,9 +84,8 @@
                         </span>
                     </div>
                 </a>
-            </div>
-
+            </div> -->
         </div>
-
     </div>
+
 </div>
