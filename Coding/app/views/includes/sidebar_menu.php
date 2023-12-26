6ea4627968664da810c22b968481caf5876a945b
SOH HAN WEI <weihansoh2030@gmail.com>
2023-12-26 09:27:41 +0800

    Update sidebar_menu.php

--- Coding/app/views/includes/sidebar_menu.php
+++ Coding/app/views/includes/sidebar_menu.php
@@ -6,16 +6,45 @@
 									<!--begin::Menu-->
 									<div class="menu menu-column menu-rounded menu-sub-indention fw-semibold fs-6" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
 
-										<!--begin:Menu item-->
-										<div class="menu-item pt-5">
-											<!--begin:Menu content-->
-											<div class="menu-content">
-												<span class="menu-heading fw-bold text-uppercase fs-7">Posts</span>
+
+										<!--begin:Menu item (Dashboard)-->
+										<div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+											<!--begin:Menu link-->
+											<span class="menu-link">
+												<span class="menu-icon">
+													<i class="ki-duotone ki-element-7 fs-2">
+														<span class="path1"></span>
+														<span class="path2"></span>
+													</i>
+												</span>
+												<span class="menu-title">Posts Options</span>
+												<span class="menu-arrow"></span>
+											</span>
+											<!--end:Menu link-->
+											<!--begin:Menu sub-->
+											<div class="menu-sub menu-sub-accordion">
+												<!--begin:Menu item-->
+												<div class="menu-item">
+													<!--begin:Menu link-->
+													<a class="menu-link" href="<?php echo URLROOT; ?>/posts">
+														<span class="menu-bullet">
+															<span class="bullet bullet-dot"></span>
+														</span>
+														<span class="menu-title">Manage Posts</span>
+													</a>
+													<!--end:Menu link-->
+												</div>
+												<!--end:Menu item-->																				
+										
 											</div>
-											<!--end:Menu content-->
+											<!--end:Menu sub-->
 										</div>
-										<!--end:Menu item-->
-										<!--begin:Menu item-->
+										<!--end:Menu item (Dashboard)-->
+										
+
+
+										
+										<!--begin:Menu item (Post Option)-->
 										<div data-kt-menu-trigger="click" class="menu-item menu-accordion">
 											<!--begin:Menu link-->
 											<span class="menu-link">
@@ -60,7 +89,7 @@
 											</div>
 											<!--end:Menu sub-->
 										</div>
-										<!--end:Menu item-->
+										<!--end:Menu item (Post Option)-->
 									
 					
 									
