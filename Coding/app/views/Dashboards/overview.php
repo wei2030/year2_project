4fb3b3b59e5d6bc1ceb6965b121515271f899d92
SOH HAN WEI <weihansoh2030@gmail.com>
2023-12-26 12:56:43 +0800

    Update overview.php

--- Coding/app/views/Dashboards/overview.php
+++ Coding/app/views/Dashboards/overview.php
@@ -5,57 +5,49 @@
     </div>
     
     <div class="card-body">
-        
-        <div class="container">
 
-            <div class="row">
-                <div class="col">
-                    <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
-                        <div class="card-body d-flex align-items">
-                            <span class="svg-icon fs-1">
-                                <svg fill="#000000" width="800px" height="800px" viewBox="-1 0 19 19" xmlns="http://www.w3.org/2000/svg" class="cf-icon-svg"><path d="M16.417 9.583A7.917 7.917 0 1 1 8.5 1.666a7.917 7.917 0 0 1 7.917 7.917zm-4.339-3.332a.794.794 0 0 0-.791-.792h-.268v.767A1.11 1.11 0 0 1 9.91 7.334H6.99a1.11 1.11 0 0 1-1.11-1.108v-.767h-.267a.794.794 0 0 0-.792.792v7.403a.794.794 0 0 0 .792.792h5.673a.794.794 0 0 0 .791-.792zm-1.077 2.522H5.787v.792h5.214zm0 1.713H5.787v.792h5.214zm0 1.713H5.787v.791h5.214zM6.673 6.226a.318.318 0 0 0 .317.317h2.92a.318.318 0 0 0 .317-.317v-.914a.318.318 0 0 0-.316-.317h-.557v-.843a.318.318 0 0 0-.317-.316H7.864a.318.318 0 0 0-.317.316v.843H6.99a.318.318 0 0 0-.317.317zm1.384-1.581a.396.396 0 1 1 .396.396.396.396 0 0 1-.396-.396z"/></svg>
-                            </span>
+        <div class="row">
 
-                            <span class="ms-3 text-gray-700 parent-hover-primary fs-6 fw-bold">
-                                Example link title
-                            </span>
-                        </div>
-                    </a>
-                </div>
+            <div class="col-md-4">
+                <!-- Link to Manage Activity -->
+                <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
+                    <div class="card-body d-flex align-items">
+                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/activity icon.png"/>
 
-                <div class="col">
-                    <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
-                        <div class="card-body d-flex align-items">
-                            <span class="svg-icon fs-1">
-                                ...
-                            </span>
+                        <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Manage Activity
+                        </span>
+                    </div>
+                </a>
+            </div>
 
-                            <span class="ms-3 text-gray-700 parent-hover-primary fs-6 fw-bold">
-                                Example link title
-                            </span>
-                        </div>
-                    </a>
-                </div>
+            <div class="col-md-4">
+                <!-- Link to List of Reward -->
+                <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
+                    <div class="card-body d-flex align-items">
+                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/reward icon.png"/>
 
-                <div class="col">
-                    <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
-                        <div class="card-body d-flex align-items">
-                            <span class="svg-icon fs-1">
-                                ...
-                            </span>
+                        <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;List of Rewards
+                           </span>
+                    </div>
+                </a>
+            </div>
 
-                            <span class="ms-3 text-gray-700 parent-hover-primary fs-6 fw-bold">
-                                Example link title
-                            </span>
-                        </div>
-                    </a>
-                </div>
+            <div class="col-md-4">
+                <!-- Link to Leaderboard -->
+                <a href="#" class="card hover-elevate-up shadow-sm parent-hover">
+                    <div class="card-body d-flex align-items">
+                        <img class="mw-100 mh-300px card-rounded-bottom" style="width: 50px;" alt="" src="<?php echo URLROOT ?>/public/assets/media/svg/dashboard/leaderboard icon.png"/>
+
+                        <span class="text-gray-700 parent-hover-primary fs-1 fw-bold" style="padding-top: 8px">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Leaderboard
+                        </span>
+                    </div>
+                </a>
             </div>
 
         </div>
 
-    </div>
-    <div class="card-footer">
-        Footer
     </div>
 </div>
