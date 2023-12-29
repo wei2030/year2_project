04942f828e758b0dc8b9ec09594eadcb5a4789f7
SOH HAN WEI <weihansoh2030@gmail.com>
2023-12-29 21:20:16 +0800

    Update update.php

--- Coding/app/views/Badges/update.php
+++ Coding/app/views/Badges/update.php
@@ -1,27 +1,37 @@
 <div class="card shadow-sm">
+
     <div class="card-header">
-        <h3 class="card-title">Update Post</h3>
+
+        <h3 class="card-title">Update Badge</h3>
+
         <div class="card-toolbar">
             <?php if(isLoggedIn()): ?>
-            <a href="<?php echo URLROOT;?>/posts" class="btn btn-light-primary">Manage Posts</a>
+                <a href="<?php echo URLROOT;?>/badges" class="btn btn-light-primary"><i class="fa fa-home"></i></a>
+                <!-- <a href="<?php echo URLROOT;?>/posts" class="btn btn-light-primary">Manage Posts</a> -->
             <?php endif; ?>
         </div>
+
     </div>
+
     <div class="card-body">
-        <form action="<?php echo URLROOT; ?>/posts/update/<?php echo $data['post']->id ?>" method="POST">
+
+        <form action="<?php echo URLROOT; ?>/badges/update/<?php echo $data['badge']->badge_id ?>" method="POST">
             <div class="mb-10">
-                <label for="exampleFormControlInput1" class="required form-label">Title</label>
-                <input type="text" name="title" class="form-control form-control-solid"
-                    value="<?php echo $data['post']->title ?>" required />
+                <label for="exampleFormControlInput1" class="required form-label">Badge Name</label>
+                <input type="text" name="badge_name" class="form-control form-control-solid" value="<?php echo $data['badge']->badge_name ?>" required />
             </div>
 
             <div class="mb-10">
-                <label for="exampleFormControlInput1" class="form-label">Content</label>
+                <label for="exampleFormControlInput1" class="required form-label">Badge Description</label>
                 <div class="position-relative">
-                    <div class="required position-absolute top-0"></div>
-                    <textarea name="body" class="form-control" aria-label="With textarea"
-                        required><?php echo $data['post']->body;?></textarea>
+                    <div class="position-absolute top-0"></div>
+                    <textarea name="badge_desc" class="form-control" aria-label="With textarea" required><?php echo $data['badge']->badge_desc;?></textarea>
                 </div>
+            </div>
+
+            <div class="mb-10">
+                <label for="exampleFormControlInput1" class="required form-label">Icon</label>
+                <input type="text" name="icon" class="form-control form-control-solid" value="<?php echo $data['badge']->icon ?>" required />
             </div>
 
             <button type="submit" class="btn btn-primary font-weight-bold">Submit</button>
@@ -29,7 +39,5 @@
         </form>
 
     </div>
-    <div class="card-footer">
-        Footer
-    </div>
+
 </div>
