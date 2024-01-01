962e1664576ce594bb6f19e5f35fe45a4fee8db6
LEE SOON DER <129196395+LeeSoonDer@users.noreply.github.com>
2024-01-01 14:44:42 +0800

    Update Activities.php

--- Coding/app/models/Activities.php
+++ Coding/app/models/Activities.php
@@ -30,16 +30,23 @@ class Activities
     }
 
     public function updateActivity($data)
-    {
-        $this->db->query('UPDATE posts SET name = :name, venue = :venue, desc = :desc, WHERE ac_id = :ac_id');
+{
+    $this->db->query('UPDATE activity SET name = :name, venue = :venue, `desc` = :desc WHERE ac_id = :ac_id');
 
-        $this->db->bind(':ac_id', $data['ac_id']);
-        $this->db->bind(':name', $data['name']);
-        $this->db->bind(':venue', $data['venue']);
+    $this->db->bind(':ac_id', $data['ac_id']);
+    $this->db->bind(':name', $data['name']);
+    $this->db->bind(':venue', $data['venue']);
+
+    // Check if the "desc" key is present before binding
+    if (array_key_exists('desc', $data)) {
         $this->db->bind(':desc', $data['desc']);
-
-        
     }
+
+    // Execute the query and handle the result (if needed)
+    $this->db->execute();
+}
+
+    
 
     public function addActivity($data)
     {
@@ -59,6 +66,22 @@ class Activities
         {
             return false;
         }
+    }
+
+    public function deleteActivity($ac_id){
+        $this->db->query('DELETE FROM activity WHERE ac_id = :ac_id');
+
+        $this->db->bind(':ac_id', $ac_id);
+
+        if ($this->db->execute())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
     
 }
