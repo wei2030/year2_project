c850fb52be86592f6247f9b0a48afce7bb1c5972
Lim Jing Yong <jingyong0814@gmail.com>
2024-01-03 17:04:32 +0800

    Update Accounts.php

--- Coding/app/controllers/Accounts.php
+++ Coding/app/controllers/Accounts.php
@@ -30,7 +30,7 @@ class Accounts extends Controller
         $this->view('accounts/index');
     }
 
-    public function st_edit_profile()
+    public function edit_profile()
     {
 
         //check for post from form
@@ -128,94 +128,9 @@ class Accounts extends Controller
                     ];
                 }
 
-            }
-
-
-          if ($_POST['update_student']) {
-                if ($this->accountModel->updateStudentProfile($data)) {
-                    header("Location: " . URLROOT . "/accounts");
-                } else {
-                    die("Something went wrong, please try again!");
-                }
-            } else {
-                $this->view('accounts');
-            }
-        } // end of if statement 
-
-        $studentProfile = $this->accountModel->studentProfile();
-
-        $data = [
-            'studentProfile' => $studentProfile
-        ];
-
-        $this->view('accounts/index', $data);
-    }
-
-    public function lc_edit_profile()
-    {
-
-        //check for post from form
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') { //if server request open
-
-            //sanitize post data
-            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
-            // Check if file was uploaded without errors
-            if (isset($_FILES["file"]) && $_FILES["file"]["error"] == 0) {
-                $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
-                $filename = $_FILES["file"]["name"];
-                $filetype = $_FILES["file"]["type"];
-                $filesize = $_FILES["file"]["size"];
-
-                $fileExt = explode('.', $filename);
-                $fileActualExt = strtolower(end($fileExt));
-
-                $ext = pathinfo($filename, PATHINFO_EXTENSION);
-                if (!array_key_exists($ext, $allowed)){
-                    $_SESSION['failed'] = "Error: You cannot upload files of this type!";
-                    header("Location: " . URLROOT . "/accounts/edit_profile");
-                }
-
-                $username = $_SESSION['email'];
-                $maxsize = 5 * 1024 * 1024;
-                if ($filesize > $maxsize){
-                    $_SESSION['failed'] = "Error: File size is larger than the allowed limit.";
-                            header("Location: " . URLROOT . "/accounts/edit_profile");
-                } 
-                $location = "images/users/" . $username;
-
-                if (in_array($filetype, $allowed)) {
-
-                    if (file_exists($location . $filename)) {
-                        echo $filename . " is already exists.";
-                    } else {
-                        
-                            # create directory if not exists in upload/ directory
-                            if (!is_dir($location)) {
-                                //mkdir($location, 0755);
-                                mkdir('images/users/' . $username, 0777, true);
-                            }
-
-                            $fileNameNew = uniqid('', true) . "." . $fileActualExt;
-
-                            $location .= "/" . $fileNameNew;
-
-                            move_uploaded_file($_FILES['file']['tmp_name'], $location);
-                    }
-                } else {
-                    $_SESSION['failed'] = "Error: There was an error uploading your file!";
-                        header("Location: " . URLROOT . "/accounts/edit_profile");
-                }
-            } else {
-
-                $_SESSION['failed'] = "Error: There was an error uploading your file!";
-                        header("Location: " . URLROOT . "/accounts/edit_profile");
-              
-            }
-
-            // $_POST['update_lecturer'] hidden value from form
-            if ($_POST['update_lecturer']) {
-
+            } 
+            else if ($_POST['update_lecturer'])
+            {
                 if (isset($_FILES["file"]) && $_FILES["file"]["error"] == 0) {
 
                     $data = [
@@ -248,28 +163,43 @@ class Accounts extends Controller
                
                     ];
                 }
-
             }
 
 
-          if ($_POST['update_lecturer']) {
+          if ($_POST['update_student']) {
+                if ($this->accountModel->updateStudentProfile($data)) {
+                    header("Location: " . URLROOT . "/accounts");
+                } else {
+                    die("Something went wrong, please try again!");
+                }
+            }
+          else if($_POST['update_lecturer']){
                 if ($this->accountModel->updateLecturerProfile($data)) {
                     header("Location: " . URLROOT . "/accounts");
                 } else {
                     die("Something went wrong, please try again!");
                 }
+
             } else {
                 $this->view('accounts');
             }
         } // end of if statement 
 
-        $lecturerProfile = $this->accountModel->lecturerProfile();
+        $studentProfile = $this->accountModel->studentProfile();
 
         $data = [
+            'studentProfile' => $studentProfile
+        ];
+
+        $lecturerProfile = $this->accountModel->lecturerProfile();
+
+        $data_2 = [
             'lecturerProfile' => $lecturerProfile
         ];
 
-        $this->view('accounts/index', $data);
+        $this->view('accounts/index', $data, $data_2);
     }
+
+
 }
 ?>
