f8fc76bd07cf2a831f104daf899669fd9067a385
SOH HAN WEI <weihansoh2030@gmail.com>
2024-01-05 01:24:22 +0800

    Update Skill.php

--- Coding/app/models/Skill.php
+++ Coding/app/models/Skill.php
@@ -11,6 +11,7 @@
 
         }
 
+        // Admin CRUD part
         public function findAllSkills() {
 
             $this->db->query('SELECT * FROM skills');
@@ -89,6 +90,49 @@
             }
 
         }
+
+        public function assignSkills(){ // assign skills to student
+
+            $this->db->query('INSERT INTO stud_skills (st_id, skill_id) VALUES (:st_id, :skill_id)');
+        
+            $this->db->bind(':st_id', $data['st_id']);
+            $this->db->bind(':skill_id', $data['skill_id']);
+
+            //execute function
+            if ($this->db->execute()) {
+
+                return true;
+
+            } else {
+
+                return false;
+
+            }
+
+        }
+        // End of admin CRUD part
+
+        // Student view part
+        public function findStudentSkills(){
+
+            $this->db->query(
+
+                'SELECT s.skill_name, s.skill_desc, s.skill_dir
+                FROM st_profile AS st 
+                INNER JOIN stud_skills AS ss ON st.st_id = ss.st_id
+                INNER JOIN skills AS s ON ss.skill_id = s.skill_id
+                WHERE st_email = :email'
+
+            );
+
+            $this->db->bind(':email', $_SESSION['email']);
+    
+            $result = $this->db->resultSet();
+
+            return $result;
+
+        }
+        // End of student view part
 
     }
 
