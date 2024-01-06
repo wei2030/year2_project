24bc1b60c57343b67a331e2fa6ca711c6770e1b5
Marcus Joey Sayner <129279940+xmqrz@users.noreply.github.com>
2024-01-06 22:52:13 +0800

    Delete app directory

--- app/views/Activity/index.php
+++
@@ -1,8 +0,0 @@
-<?php require APPROOT . '/views/includes/head_metronic.php';?>
-<?php require APPROOT . '/views/includes/begin_app.php';?>
-
-<?php require APPROOT . '/views/activity/layout.php'; ?>  
-                
-<?php require APPROOT . '/views/includes/end_app.php'; ?>
-<?php require APPROOT . '/views/includes/footer_metronic.php';?>
-
