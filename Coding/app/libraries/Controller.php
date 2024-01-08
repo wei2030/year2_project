4aae96bc0c63f0062814c13e82bcbd119a2022da
SOH HAN WEI <weihansoh2030@gmail.com>
2024-01-08 15:46:24 +0800

    Update Controller.php

--- Coding/app/libraries/Controller.php
+++ Coding/app/libraries/Controller.php
@@ -9,7 +9,9 @@
         }
 
         //Load the view (checks for the file)
-        public function view($view, $data = []) {
+        public function view($view, $data = [], $data_2 = [], $data_3 = [], $data_4 = [], $data_5 = [],
+                                $data_6 = [], $data_7 = [], $data_8 = [], $data_9 = [], $data_10 = [],
+                                $data_11 = [], $data_12 = []) {
             if (file_exists('../app/views/' . $view . '.php')) {
                 require_once '../app/views/' . $view . '.php';
             } else {
@@ -17,3 +19,4 @@
             }
         }
     }
+?>
