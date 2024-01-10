90a5269c217c3d888389c7803fe33bbfcacbd834
Lim Jing Yong <jingyong0814@gmail.com>
2024-01-10 23:43:17 +0800

    navbar fix

--- Coding/app/views/includes/st_navbar.php
+++ Coding/app/views/includes/st_navbar.php
@@ -130,56 +130,7 @@
 										<div class="separator my-2"></div>
 										<!--end::Menu separator-->
 										<!--end::Menu item-->
-										<!--begin::Menu item-->
-										<div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
-											<a href="#" class="menu-link px-5">
-												<span class="menu-title position-relative">Language 
-												<span class="fs-8 rounded bg-light px-3 py-2 position-absolute translate-middle-y top-50 end-0">English 
-												<img class="w-15px h-15px rounded-1 ms-2" src="<?php echo URLROOT ?>/public/assets/media/flags/united-states.svg" alt="" /></span></span>
-											</a>
-											<!--begin::Menu sub-->
-											<div class="menu-sub menu-sub-dropdown w-175px py-4">
-												<!--begin::Menu item-->
-												<div class="menu-item px-3">
-													<a href="account/settings.html" class="menu-link d-flex px-5 active">
-													<span class="symbol symbol-20px me-4">
-														<img class="rounded-1" src="<?php echo URLROOT ?>/public/assets/media/flags/united-states.svg" alt="" />
-													</span>English</a>
-												</div>
-												<!--end::Menu item-->
-												<!--begin::Menu item-->
-												<div class="menu-item px-3">
-													<a href="account/settings.html" class="menu-link d-flex px-5">
-													<span class="symbol symbol-20px me-4">
-														<img class="rounded-1" src="<?php echo URLROOT ?>/public/assets/media/flags/malaysia.svg" alt="" />
-													</span>Malay</a>
-												</div>
-												<!--end::Menu item-->
-												<!--begin::Menu item-->
-												<div class="menu-item px-3">
-													<a href="account/settings.html" class="menu-link d-flex px-5">
-													<span class="symbol symbol-20px me-4">
-														<img class="rounded-1" src="<?php echo URLROOT ?>/public/assets/media/flags/china.svg" alt="" />
-													</span>Chinese</a>
-												</div>
-												<!--end::Menu item-->
-												<!--begin::Menu item-->
-												<div class="menu-item px-3">
-													<a href="account/settings.html" class="menu-link d-flex px-5">
-													<span class="symbol symbol-20px me-4">
-														<img class="rounded-1" src="<?php echo URLROOT ?>/public/assets/media/flags/india.svg" alt="" />
-													</span>Indo</a>
-												</div>
-												<!--end::Menu item-->
-											</div>
-											<!--end::Menu sub-->
-										</div>
-										<!--end::Menu item-->
-										<!--begin::Menu item-->
-										<div class="menu-item px-5 my-1">
-											<a href="account/settings.html" class="menu-link px-5">Account Settings</a>
-										</div>
-										<!--end::Menu item-->
+									
 										<!--begin::Menu item-->
 										<div class="menu-item px-5">
 										
